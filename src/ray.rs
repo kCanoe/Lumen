bdc39dfb17307cbda9a04d7b074aa57cf6a0af4b
kCanoe <kanoalathamj@gmail.com>
2025-02-16 14:17:51 -0800

    created hittable trait and abstracted away from hard coding in ray_color method

--- src/ray.rs
+++ src/ray.rs
@@ -9,6 +9,63 @@ pub struct Ray {
     pub direction: Vec3,
 }
 
+pub struct HitRecord {
+    pub point: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+}
+
+pub struct Sphere {
+    pub radius: f64,
+    pub center: Point3,
+}
+
+impl HitRecord {
+    pub fn new() -> Self {
+        HitRecord {
+            point: Point3::new(0.0, 0.0, 0.0),
+            normal: Vec3::new(1.0, 1.0, 1.0),
+            t: 0.0,
+        }
+    }
+}
+
+impl Sphere {
+    pub fn new(radius: f64, cx: f64, cy: f64, cz: f64) -> Self {
+        Sphere {
+            radius: radius,
+            center: Point3::new(cx, cy, cz),
+        } 
+    }
+}
+
+trait Hittable {
+    fn hit(&self, ray: &Ray, record: &mut HitRecord) -> bool;
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, record: &mut HitRecord) -> bool {
+        let oc = Vec3::from_point(self.center - ray.origin);
+        
+        let a = ray.direction * ray.direction;
+        let h = ray.direction * oc;
+        let c = (oc * oc) - (self.radius * self.radius); 
+        let discriminant = h*h - a*c; 
+
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let root = (h - discriminant.sqrt()) / a;
+
+        record.t = root;
+        record.point = ray.at(root);
+        record.normal = Vec3::from_point((ray.at(root) - self.center) / self.radius);
+
+        return true;
+    }
+}
+
 impl Ray {
     pub fn new(origin: Point3, direction: Vec3) -> Self {
         Ray {
@@ -24,45 +81,30 @@ impl Ray {
         }
     }
 
-    pub fn at(&self, t: f64) -> Vec3 {
-        Vec3::from_point(self.origin + t * self.direction)
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin + t * self.direction
     }
 
+    pub fn ray_color(ray: Ray) -> Color {
+        let sphere = Sphere::new(0.4, 0.0, 0.0, -1.0);
 
-    pub fn hit_sphere(&self, center: Point3, radius: f64) -> f64 {
-        let oc = Vec3::from_point(center - self.origin);
+        let mut record = HitRecord::new();
         
-        let a = self.direction * self.direction;
-        let h = self.direction * oc;
-        let c = (oc * oc) - (radius * radius); 
-        let discriminant = h*h - a*c; 
-        
-        if discriminant < 0.0 {
-            return -1.0;
-        } else {
-            let t: f64 = (h - discriminant.sqrt()) /  a;
-            return t;
+        let hit: bool = sphere.hit(&ray, &mut record);
+
+        match hit {
+            true => {
+                let N: Vec3 = 0.5 * (record.normal + Vec3::new(1.0, 1.0, 1.0));
+                Color::new(N.x, N.y, N.z)
+            }
+            false => {
+                let unit_direction = Vec3::unit_vector(ray.direction);
+                let a: f64 = 0.5 * (unit_direction.y + 1.0);
+                let start_value = Color::new(1.0, 1.0, 1.0);
+                let end_value = Color::new(0.5, 0.7, 1.0); 
+                (1.0-a) * start_value + a * end_value
+            }
         }
-    } 
-
-    pub fn ray_color(r: Ray) -> Color {
-        let origin = Point3::new(0.0, 0.0, -1.0);
-
-        let t = r.hit_sphere(origin, 0.45);
-
-        if t > 0.0 {
-            let N = Vec3::unit_vector(r.at(t) - Vec3::from_point(origin));
-            let normal_shade = Color::new(N.x+1.0, N.y+1.0, N.z+1.0);
-            return 0.5 * normal_shade;
-        }
-
-        let unit_direction = Vec3::unit_vector(r.direction);
-        let a: f64 = 0.5 * (unit_direction.y + 1.0);
-
-        let start_value = Color::new(1.0, 1.0, 1.0);
-        let end_value = Color::new(0.5, 0.7, 1.0); 
-
-        (1.0-a) * start_value + a * end_value
     }
 }
 
