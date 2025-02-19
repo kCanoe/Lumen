0af6d328e3d3008bc9c057b171fb112e11a07db6
kCanoe <kanoalathamj@gmail.com>
2025-02-19 13:24:39 -0800

    multi threading implemented in place and safely

--- src/image.rs
+++ src/image.rs
@@ -10,7 +10,7 @@ pub struct Pixel {
 }
 
 pub struct Image {
-    pub data: Vec<Vec<Pixel>>,
+    pub data: Vec<Pixel>,
     pub rows: usize,
     pub cols: usize,
 }
@@ -23,6 +23,14 @@ impl Pixel {
     pub fn to_byte(value: f64) -> u8 {
         (255.999 * value.max(0.0).min(1.0)) as u8
     }
+
+    pub fn from_vec(v: Vec3) -> Self {
+        Pixel {
+            r: Self::to_byte(v.x),
+            g: Self::to_byte(v.y),
+            b: Self::to_byte(v.z),
+        }
+    }
 }
 
 impl fmt::Display for Pixel {
@@ -34,20 +42,18 @@ impl fmt::Display for Pixel {
 impl Image {
     pub fn new(width: usize, height: usize) -> Self {
         Image {
-            data: vec![vec![Pixel::new(0, 0, 0); width]; height],
+            data: vec![Pixel::new(0, 0, 0); width * height],
             rows: height,
             cols: width,
         }
     }
 
     pub fn get(&self, i: usize, j: usize) -> &Pixel {
-        &self.data[i][j]
+        &self.data[i * self.cols + j]
     }
 
-    pub fn set(&mut self, i: usize, j: usize, color: &Vec3) {
-        self.data[i][j].r = Pixel::to_byte(color.x);
-        self.data[i][j].g = Pixel::to_byte(color.y);
-        self.data[i][j].b = Pixel::to_byte(color.z);
+    pub fn set(&mut self, i: usize, j: usize, p: Pixel) {
+        self.data[i * self.cols + j] = p;
     }
 
     pub fn print(&self) {
