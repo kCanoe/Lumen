9a87a341d2689593b1f3ac37ec083dcb951c3db1
kCanoe <kanoalathamj@gmail.com>
2025-03-22 14:14:49 -0700

    formatting and new scene

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,33 @@ use lumen::rendering::*;
 use lumen::Material;
 use lumen::ObjectList;
 
-fn setup() -> (Camera, ObjectList, usize) {
-    let ground = Material::new_diffuse(0.5, 0.5, 0.5);
-    let mat1 = Material::new_dielectric(1.50);
-    let mat2 = Material::new_diffuse(0.2, 0.5, 0.7);
-    let mat3 = Material::new_metal(0.2, 0.7, 0.5, 0.0);
-    let green = Material::new_diffuse(0.2, 0.8, 0.2);
-
+fn make_cube_array() -> ObjectList {
     let mut objects = ObjectList::new();
+    for i in 0..4 {
+        for j in 0..4 {
+            let (cx, cy) = (3.0 * i as f64, 3.0 * j as f64);
+            let mat = Material::new_metal(0.5, 0.5, 0.7, 0.0);
+            objects.add_sphere(1.0, cx, 0.5, cy, mat);
+        }
+    }
+    let ground = Material::new_diffuse(0.5, 0.5, 0.5);
     objects.add_sphere(1000.0, 0.0, -1000.0, 0.0, ground);
-    objects.add_sphere(1.0, 0.0, 1.0, 0.0, mat1);
-    objects.add_sphere(1.0, -2.0, 1.0, 0.0, mat2);
-    objects.add_sphere(1.0, 2.0, 1.0, 0.0, mat3);
-    objects.add_cube(1.0, 2.0, 1.0, 1.0, green);
+    objects
+}
+
+fn setup() -> (Camera, ObjectList, usize) {
+    let objects = make_cube_array();
 
     let camera = CameraBuilder::new()
         .resolution(1024, 576)
         .vfov(90.0)
-        .target(0.0, 0.0, -1.0)
-        .position(3.0, 2.0, 3.0)
+        .target(6.0, 0.0, 6.0)
+        .position(8.0, 8.0, 8.0)
         .upward(0.0, 1.0, 0.0)
         .samples(10)
         .max_depth(10)
         .build();
-    (camera, objects, 8)
+    (camera, objects, 32)
 }
 
 fn main() -> std::io::Result<()> {
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,10 +1,10 @@
 // modules
 pub mod ray;
-pub mod vec3;
 pub mod util;
+pub mod vec3;
 
 // flatten
 pub use ray::Interval;
 pub use ray::Ray;
-pub use vec3::Vec3;
 pub use util::lerp;
+pub use vec3::Vec3;
